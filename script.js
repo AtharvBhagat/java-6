385b1fc531454b0dcb11a79cf6cc5588850ee977
AtharvBhagat <159274676+AtharvBhagat@users.noreply.github.com>
2025-01-02 20:54:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var num1=23;
+    var num2=10
+    document.getElementById("result").innerHTML=num1*num2;
+    document.getElementById("demo").firstChild.nodeValue;
+    document.getElementById("result1").innerHTML=num1+num2;
+    document.getElementById("demo").childNodes[0].nodeValue;
+}
